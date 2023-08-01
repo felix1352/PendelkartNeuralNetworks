d15aca627d888ee692c13307374381e81cd939f9
hoehl <andreas.hoehl@tu-darmstadt.de>
2023-08-01 14:59:30 +0200

    Implemented (quick and not beautiful) that only angle and position is measurable. Still works.

--- temp_andi.py
+++ temp_andi.py
@@ -13,6 +13,10 @@ from dynamics import get_dxdt
 from scipy.integrate import solve_ivp
 import numpy as np
 from pathlib import Path
+from keras.models import load_model
+
+STATES_MEASURABLE = False
+HISTORY_SIZE = 5  # number of past angles that are considered
 
 
 def revert_modulo(angles, threshold=np.pi):
@@ -31,6 +35,40 @@ def revert_modulo(angles, threshold=np.pi):
 
 
 def get_observation_and_target(state_sequence, force_sequence):
+    if STATES_MEASURABLE:
+        return get_observation_and_target_state_measurable(state_sequence, force_sequence)
+    else:
+        return get_observation_and_target_history_angles(state_sequence, force_sequence)
+
+
+def get_input_and_output_size():
+    if STATES_MEASURABLE:
+        return 6, 5
+    else:
+        return 3 * HISTORY_SIZE + 1, 3
+
+
+def get_observation_and_target_history_angles(state_sequence, force_sequence):
+    X = []
+    for i in range(HISTORY_SIZE - 1, -1, -1):
+        X.append(np.sin(state_sequence[i:-HISTORY_SIZE + i, 0]))
+    for i in range(HISTORY_SIZE - 1, -1, -1):
+        X.append(np.cos(state_sequence[i:-HISTORY_SIZE + i, 0]))
+    for i in range(HISTORY_SIZE - 1, -1, -1):
+        X.append(state_sequence[i:-HISTORY_SIZE + i, 2])
+    X.append(force_sequence[HISTORY_SIZE - 1:-1])
+    X = np.array(X).transpose()  # cos(phi_k), cos(phi_k-1), ..., cos(phi_k-History-1), sin(phi_k), ....
+
+    Y = np.array([
+        np.sin(state_sequence[HISTORY_SIZE:, 0]),
+        np.cos(state_sequence[HISTORY_SIZE:, 0]),
+        state_sequence[HISTORY_SIZE:, 2],
+    ]).transpose()
+    Y = Y - X[:, [HISTORY_SIZE * i for i in range(3)]]  # only learn the change of the state
+    return X, Y
+
+
+def get_observation_and_target_state_measurable(state_sequence, force_sequence):
     X_part = np.array([
         np.sin(state_sequence[:-1, 0]),  # sin(phi)
         np.cos(state_sequence[:-1, 0]),  # cos(phi)
@@ -48,6 +86,39 @@ def get_observation_and_target(state_sequence, force_sequence):
     ]).transpose()
     Y_part = Y_part - X_part[:, :-1]  # only learn the change of the state
     return X_part, Y_part
+
+
+def forward_prediction_FFNN(model, force, forward_simulation_result, normalisation_factors):
+    mean_X, std_X, mean_Y, std_Y = normalisation_factors
+
+    if STATES_MEASURABLE:
+        forward_prediction = [forward_simulation_result[0]]
+        for i in range(len(forward_simulation_result) - 1):
+            current_input = get_observation_from_state(forward_prediction[-1], force[i])
+            current_input_norm = (current_input - mean_X) / std_X
+            Y_pred_norm = model(current_input_norm).numpy().flatten()
+            Y_pred = Y_pred_norm * std_Y + mean_Y
+            forward_prediction.append(get_state_from_prediction(current_input, Y_pred))
+    else:
+        forward_prediction = []
+        for i in range(HISTORY_SIZE):
+            forward_prediction.append(forward_simulation_result[i])
+        for i in range(len(forward_simulation_result) - HISTORY_SIZE):
+            last_state_sequence = np.array(forward_prediction[i:i + HISTORY_SIZE])
+            last_state_sequence = np.vstack((last_state_sequence, np.zeros((1, 4)) * np.nan))
+            current_input, _ = get_observation_and_target_history_angles(last_state_sequence,
+                                                                         np.append(force[i:i + HISTORY_SIZE], np.nan))
+            current_input_norm = (current_input - mean_X) / std_X
+            Y_pred_norm = model(current_input_norm).numpy().flatten()
+            Y_pred = Y_pred_norm * std_Y + mean_Y
+            Y = (Y_pred + current_input[:, [HISTORY_SIZE * i for i in range(3)]]).flatten()
+            angle = np.arctan2(Y[0], Y[1])
+            predicted_state = np.array([angle, np.nan, Y[2], np.nan])
+            forward_prediction.append(predicted_state)
+
+    forward_prediction = np.array(forward_prediction)
+    forward_prediction[:, 0] = revert_modulo(forward_prediction[:, 0])
+    return forward_prediction
 
 
 def get_observation_from_state(state, force):
@@ -91,10 +162,9 @@ def main():
 
     state_names = ['phi', 'dphi', 's', 'ds']
     n = len(state_names)
-    number_of_simulations = 100
+    number_of_simulations = 1000
 
-    input_dim = 6
-    output_dim = 5
+    input_dim, output_dim = get_input_and_output_size()
 
     model = Sequential()
     model.add(Dense(units=200, input_shape=(input_dim,), activation='relu'))
@@ -129,10 +199,11 @@ def main():
 
     X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.2, random_state=42)
     filepath = Path() / 'ModelsFFnn' / f'cartpole_NN.hdf5'
-    checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=False,
+    checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=False,
                                  mode='min')
-    history = model.fit(X_train, Y_train, epochs=10, batch_size=128, callbacks=[checkpoint],
+    history = model.fit(X_train, Y_train, epochs=30, batch_size=256, callbacks=[checkpoint],
                         validation_data=(X_val, Y_val), verbose=2)
+    model = load_model(filepath)
 
     Y_val_pred = model(X_val).numpy()
     mse_val = (1 / Y_val.shape[1] * np.sum((Y_val_pred - Y_val) ** 2, axis=1)).mean()
@@ -140,15 +211,8 @@ def main():
 
     x0 = np.array([0.4, 0, 0, 0])
     forward_simulation_result = solve_ivp(f, t_span=[t_start, t_end], y0=x0, t_eval=t_eval)
-    forward_prediction = [x0]
-    for i in range(len(t_eval) - 1):
-        current_input = get_observation_from_state(forward_prediction[-1], F_func(t_eval[i]))
-        current_input_norm = (current_input - mean_X) / std_X
-        Y_pred_norm = model(current_input_norm).numpy().flatten()
-        Y_pred = Y_pred_norm * std_Y + mean_Y
-        forward_prediction.append(get_state_from_prediction(current_input, Y_pred))
-    forward_prediction = np.array(forward_prediction)
-    forward_prediction[:, 0] = revert_modulo(forward_prediction[:, 0])
+    forward_prediction = forward_prediction_FFNN(model, F_func(t_eval), forward_simulation_result.y.transpose(),
+                                                 (mean_X, std_X, mean_Y, std_Y))
 
     fig, axs = plt.subplots(n)
     fig.suptitle('Forward Simulation and Network prediction')
