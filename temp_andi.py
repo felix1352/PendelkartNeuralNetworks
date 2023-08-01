07add9e4292d6641d37a5fa741cbf5b6a76eba1d
hoehl <andreas.hoehl@tu-darmstadt.de>
2023-08-01 19:10:33 +0200

    now the force can be selected arbitrarily. This makes the task significantly harder!

--- temp_andi.py
+++ temp_andi.py
@@ -14,9 +14,32 @@ from scipy.integrate import solve_ivp
 import numpy as np
 from pathlib import Path
 from keras.models import load_model
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
 
-STATES_MEASURABLE = False
+STATE_NAMES = ['phi', 'dphi', 's', 'ds']
+
+STATES_MEASURABLE = True
 HISTORY_SIZE = 5  # number of past angles that are considered
+
+TS = 0.01
+CART_POLE_PARAMETER = {
+    'g': 9.81,
+    'M': 0.7,
+    'm': 0.221,
+    'l': 0.5,
+    'b': 0.02,
+    'c': 1
+}
+NUMBER_OF_TRAINING_SIMULATIONS = 200
+RANDOM_INITIAL_STATE = True
+EPOCHS = 30
+
+DX_DT = get_dxdt()
+
+
+def rhs_cartpole(t: int, x: np.ndarray, F) -> np.ndarray:
+    return DX_DT(t, x, parameter=CART_POLE_PARAMETER, F=F)
 
 
 def revert_modulo(angles, threshold=np.pi):
@@ -56,7 +79,7 @@ def get_observation_and_target_history_angles(state_sequence, force_sequence):
         X.append(np.cos(state_sequence[i:-HISTORY_SIZE + i, 0]))
     for i in range(HISTORY_SIZE - 1, -1, -1):
         X.append(state_sequence[i:-HISTORY_SIZE + i, 2])
-    X.append(force_sequence[HISTORY_SIZE - 1:-1])
+    X.append(force_sequence[HISTORY_SIZE - 1:])
     X = np.array(X).transpose()  # cos(phi_k), cos(phi_k-1), ..., cos(phi_k-History-1), sin(phi_k), ....
 
     Y = np.array([
@@ -75,7 +98,7 @@ def get_observation_and_target_state_measurable(state_sequence, force_sequence):
         state_sequence[:-1, 1],  # dphi
         state_sequence[:-1, 2],  # s
         state_sequence[:-1, 3],  # ds
-        force_sequence[:-1]  # F
+        force_sequence  # F
     ]).transpose()
     Y_part = np.array([
         np.sin(state_sequence[1:, 0]),  # sin(phi)
@@ -107,7 +130,7 @@ def forward_prediction_FFNN(model, force, forward_simulation_result, normalisati
             last_state_sequence = np.array(forward_prediction[i:i + HISTORY_SIZE])
             last_state_sequence = np.vstack((last_state_sequence, np.zeros((1, 4)) * np.nan))
             current_input, _ = get_observation_and_target_history_angles(last_state_sequence,
-                                                                         np.append(force[i:i + HISTORY_SIZE], np.nan))
+                                                                         force[i:i + HISTORY_SIZE])
             current_input_norm = (current_input - mean_X) / std_X
             Y_pred_norm = model(current_input_norm).numpy().flatten()
             Y_pred = Y_pred_norm * std_Y + mean_Y
@@ -142,28 +165,91 @@ def get_state_from_prediction(X, Y_pred):
                      Y[4]]).reshape((4,))
 
 
+def test_model(model, normalization_values):
+    fig, axs = plt.subplots(4 + 1)
+    fig.suptitle('Forward Simulation and Network prediction')
+    for j in range(1):
+        if j == 0:
+            x0 = np.array([0.4, 0, 0, 0])
+            force_vector = np.sin(np.arange(int(20 / TS)) * TS)
+            label1 = 'true_test'
+            label2 = 'pred_test'
+        else:
+            force_vector = generate_random_force_vector(1000, 1).flatten()
+            x0 = np.random.uniform(low=[0, -5, -0.5, -0.3],
+                                   high=[2 * np.pi, 5, 0.5, 0.3])
+            label1 = 'true_train_distribution'
+            label2 = 'pred_train_distribution'
+        state_sequence = simulate_cart_pole(x0, force_vector)
+        forward_prediction = forward_prediction_FFNN(model, force_vector, state_sequence, normalization_values)
+
+        n = len(STATE_NAMES)
+        t_eval = np.arange(len(force_vector) + 1) * TS
+        for i in range(n):
+            axs[i].plot(t_eval, state_sequence[:, i], label=label1)
+            axs[i].plot(t_eval, forward_prediction[:, i], label=label2)
+            axs[i].set_ylabel(STATE_NAMES[i])
+        # axs[i].set_ylim([state_sequence[:, i].min() - 0.5, state_sequence[:, i].max() + 0.5])
+        axs[-1].plot(t_eval[:-1], force_vector)
+        axs[-1].set_ylabel('force')
+        axs[0].legend()
+        # fig.tight_layout()
+
+    fig2, axs2 = plt.subplots(n)
+    fig2.suptitle('Forward Simulation prediction error')
+    for i in range(n):
+        axs2[i].plot(t_eval, state_sequence[:, i] - forward_prediction[:, i], label='true-pred')
+        axs2[i].set_ylabel(STATE_NAMES[i])
+        axs2[i].set_ylim([-0.5, 0.5])
+    axs2[0].legend()
+    fig2.tight_layout()
+
+
+def generate_training_data():
+    input_dim, output_dim = get_input_and_output_size()
+    X = np.empty((0, input_dim))
+    Y = np.empty((0, output_dim))
+
+    force_vectors = generate_random_force_vector(1000, NUMBER_OF_TRAINING_SIMULATIONS)
+    for i in range(NUMBER_OF_TRAINING_SIMULATIONS):
+        if RANDOM_INITIAL_STATE:
+            x0 = np.random.uniform(low=[0, -5, -0.5, -0.3],
+                                   high=[2 * np.pi, 5, 0.5, 0.3])
+        else:
+            x0 = np.zeros(4)
+
+        force_vector = force_vectors[i]
+        state_sequence = simulate_cart_pole(x0, force_vector)
+
+        X_part, Y_part = get_observation_and_target(state_sequence, force_vector)
+        X = np.vstack((X, X_part))
+        Y = np.vstack((Y, Y_part))
+
+    return X, Y
+
+
+def simulate_cart_pole(x0, force_vector):
+    states = [x0]
+    for i in range(len(force_vector)):
+        result_time_step = solve_ivp(rhs_cartpole, t_span=[i * TS, (i + 1) * TS], y0=states[-1],
+                                     args=(force_vector[i],))
+        states.append(result_time_step.y[:, 1])
+
+    return np.array(states)
+
+
+def generate_random_force_vector(vector_length, number_of_vectors):
+    kernel = 1.0 * RBF(length_scale=1.0, length_scale_bounds=(1e-1, 10.0))
+    gpr = GaussianProcessRegressor(kernel=kernel)
+
+    x = np.linspace(0, 10, vector_length)
+    X = x.reshape(-1, 1)
+
+    y_samples = gpr.sample_y(X, number_of_vectors, random_state=np.random.randint(0, 2 ** 32 - 1))
+    return y_samples
+
+
 def main():
-    cart_pole_parameter = {
-        'g': 9.81,
-        'M': 0.7,
-        'm': 0.221,
-        'l': 0.5,
-        'b': 0.02,
-        'c': 1
-    }
-
-    def F_func(t):
-        return np.sin(t)
-
-    dx_dt = get_dxdt()
-
-    def f(t: int, x: np.ndarray) -> np.ndarray:
-        return dx_dt(t, x, parameter=cart_pole_parameter, F=F_func(t))
-
-    state_names = ['phi', 'dphi', 's', 'ds']
-    n = len(state_names)
-    number_of_simulations = 1000
-
     input_dim, output_dim = get_input_and_output_size()
 
     model = Sequential()
@@ -174,23 +260,7 @@ def main():
 
     model.compile(optimizer='adam', loss='mse')
 
-    dt = 0.01
-    t_start = 0.0
-    t_end = 10
-    t_eval = np.arange(t_start, t_end, dt)
-
-    X = np.empty((0, input_dim))
-    Y = np.empty((0, output_dim))
-    for i in range(number_of_simulations):
-        x0 = np.random.uniform(low=[0, -5, -0.5, -0.3],
-                               high=[2 * np.pi, 5, 0.5, 0.3])
-        forward_simulation_result = solve_ivp(f, t_span=[t_start, t_end], y0=x0, t_eval=t_eval)
-
-        X_part, Y_part = get_observation_and_target(forward_simulation_result.y.transpose(),
-                                                    F_func(t_eval))
-        X = np.vstack((X, X_part))
-        Y = np.vstack((Y, Y_part))
-
+    X, Y = generate_training_data()
     mean_X, std_X = X.mean(axis=0), X.std(axis=0) + 1e-8  # add jitter to ensure std is not zero
     mean_Y, std_Y = Y.mean(axis=0), Y.std(axis=0) + 1e-8
 
@@ -201,7 +271,7 @@ def main():
     filepath = Path() / 'ModelsFFnn' / f'cartpole_NN.hdf5'
     checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=False,
                                  mode='min')
-    history = model.fit(X_train, Y_train, epochs=30, batch_size=256, callbacks=[checkpoint],
+    history = model.fit(X_train, Y_train, epochs=EPOCHS, batch_size=512, callbacks=[checkpoint],
                         validation_data=(X_val, Y_val), verbose=2)
     model = load_model(filepath)
 
@@ -209,29 +279,17 @@ def main():
     mse_val = (1 / Y_val.shape[1] * np.sum((Y_val_pred - Y_val) ** 2, axis=1)).mean()
     print(f'mse: {mse_val}')
 
-    x0 = np.array([0.4, 0, 0, 0])
-    forward_simulation_result = solve_ivp(f, t_span=[t_start, t_end], y0=x0, t_eval=t_eval)
-    forward_prediction = forward_prediction_FFNN(model, F_func(t_eval), forward_simulation_result.y.transpose(),
-                                                 (mean_X, std_X, mean_Y, std_Y))
+    test_model(model, (mean_X, std_X, mean_Y, std_Y))
 
-    fig, axs = plt.subplots(n)
-    fig.suptitle('Forward Simulation and Network prediction')
-    for i in range(n):
-        axs[i].plot(t_eval, forward_simulation_result.y[i], label='true')
-        axs[i].plot(t_eval, forward_prediction[:, i], label='pred')
-        axs[i].set_ylabel(state_names[i])
-        axs[i].set_ylim([forward_simulation_result.y[i].min() - 0.5, forward_simulation_result.y[i].max() + 0.5])
-    axs[0].legend()
-    fig.tight_layout()
+    plt.figure()
+    plt.semilogy(history.history['loss'])
+    plt.semilogy(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper right')
+    plt.show()
 
-    fig2, axs2 = plt.subplots(n)
-    fig2.suptitle('Forward Simulation prediction error')
-    for i in range(n):
-        axs2[i].plot(t_eval, forward_simulation_result.y[i] - forward_prediction[:, i], label='true-pred')
-        axs2[i].set_ylabel(state_names[i])
-        axs2[i].set_ylim([-0.5, 0.5])
-    axs2[0].legend()
-    fig2.tight_layout()
     plt.show()
 
 
